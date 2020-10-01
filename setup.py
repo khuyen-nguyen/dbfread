ce48e91aedfb4105ef63cd0fb01c00f56346977c
Zane Selvans <zane.selvans@catalyst.coop>
2020-10-01 11:54:27 -0500

    Organize repo to use Tox and modern packaging
    * Isolate package with a src/ directory
    * Set up Tox to run pytest, docs builds, and packaging.
    * Add test coverage measurement
    * Remove import of dbfread from setup.py because it will fail if/when
      dbfread has any external dependencies.
    * Update setup.py metadata
    * Remove some python 2 infrastructure.

--- setup.py
+++ setup.py
@@ -1,47 +1,59 @@
 #!/usr/bin/env python
 import os
 import sys
-import dbfread
+from pathlib import Path
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import find_packages, setup
 
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
-    sys.exit()
+test_requires = [
+    "coverage",
+    "pytest",
+    "pytest-cov",
+]
 
-elif sys.argv[-1] == "test":
-    os.system("./run_tests.py")
-    sys.exit()
+doc_requires = [
+    "sphinx>=3.0",
+    "sphinx_rtd_theme",
+]
 
-elif sys.argv[-1] == "docs":
-    os.system("sphinx-build docs/ docs/_build")
-    sys.exit()
+readme_path = Path(__file__).parent / "README.rst"
+long_description = readme_path.read_text()
 
 setup(
-    name='dbfread',
-    version=dbfread.__version__,
-    description='Read DBF Files with Python',
-    long_description=open('README.rst', 'rt').read(),
-    author=dbfread.__author__,
-    author_email=dbfread.__email__,
-    url=dbfread.__url__,
-    package_data={'': ['LICENSE']},
-    package_dir={'dbfread': 'dbfread'},
-    packages = ['dbfread'],
+    name="catalystcoop.dbfread",
+    version="2.1.0",
+    description="Read DBF Files with Python",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author="Catalyst Cooperative",
+    author_email="pudl@catalyst.coop",
+    url="https://github.com/catalyst-cooperative/dbfread",
+    maintainer="Zane A. Selvans",
+    maintainer_email="zane.selvans@catalyst.coop",
+    project_urls={
+        "Source": "https://github.com/catalyst-cooperative/dbfread",
+#        "Documentation": "https://catalystcoop-dbfread.readthedocs.io",
+        "Issue Tracker": "https://github.com/catalyst-cooperative/dbfread/issues",
+    },
+    package_data={"": ["LICENSE"]},
+    package_dir={"": "src"},
+    packages = find_packages("src"),
     include_package_data=True,
     zip_safe=True,
     install_requires=[],
-    license='MIT',
+    extras_require={
+        "doc": doc_requires,
+        "test": test_requires,
+        },
+    license="MIT",
     classifiers=(
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ),
 )
